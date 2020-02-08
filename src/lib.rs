7059f7028a1fdad3100be9c056754a7a90ae0098
Wesley Wigham <wwigham@gmail.com>
2020-02-08 04:10:03 -0800

    Initial attempt - rust/wasm toolchain fun

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+use fontdue::*;
+
+const ROBOTO_MONO_REGULAR_TTF: &[u8] = include_bytes!("../resources/RobotoMono-Regular.ttf");
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct ByteStream {
+    offset: *const u8,
+    size: usize,
+}
+
+#[wasm_bindgen]
+impl ByteStream {
+    pub fn new(bytes: &[u8]) -> ByteStream {
+        ByteStream {
+            offset: bytes.as_ptr(),
+            size: bytes.len(),
+        }
+    }
+
+    pub fn offset(&self) -> *const u8 {
+        self.offset
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+}
+
+#[wasm_bindgen]
+pub struct CharRender {
+    pub height: usize,
+    pub width: usize,
+    pub bitmap: ByteStream
+}
+
+#[wasm_bindgen]
+pub fn render(size: f32, input: char) -> CharRender {
+    let font = Font::from_bytes(ROBOTO_MONO_REGULAR_TTF, FontSettings::default()).unwrap();
+    let (metrics, bitmap) = font.rasterize(input, size);
+    CharRender{
+        height: metrics.height,
+        width: metrics.width,
+        bitmap: ByteStream::new(&bitmap)
+    }
+}
---
+++ www/index.js
@@ -0,0 +1,61 @@
+// @ts-check
+import * as wasm from "fontdue-you-see-it";
+import {memory} from "fontdue-you-see-it/fontdue_you_see_it_bg";
+
+function getCanvasA() {
+    const existing = document.getElementById("canvas-a");
+    if (existing) {
+        return /** @type {HTMLCanvasElement} */(existing);
+    }
+    const elem = document.createElement("canvas");
+    elem.id = "canvas-a";
+    document.getElementById("samples").appendChild(elem);
+    return elem;
+}
+
+function getCanvasB() {
+    const existing = document.getElementById("canvas-b");
+    if (existing) {
+        return /** @type {HTMLCanvasElement} */(existing);
+    }
+    const elem = document.createElement("canvas");
+    elem.id = "canvas-b";
+    document.getElementById("samples").appendChild(elem);
+    return elem;
+}
+
+function renderFontdueCharacter(char = "¾", size = 600) {
+    const rednerResult = wasm.render(size, char);
+    const textureRaw = new Uint8ClampedArray(memory.buffer, rednerResult.bitmap.offset(), rednerResult.bitmap.size());
+    const clampedFullColor = new Uint8ClampedArray(textureRaw.length * 4);
+    for (let i = 0; i < textureRaw.length; i++) {
+        clampedFullColor[i*4] = 255 - textureRaw[i];
+        clampedFullColor[i*4 + 1] = 255 - textureRaw[i];
+        clampedFullColor[i*4 + 2] = 255 - textureRaw[i];
+        clampedFullColor[i*4 + 3] = 255;
+    }
+    const image = new ImageData(clampedFullColor, rednerResult.width, rednerResult.height);
+    const elem = getCanvasA();
+    elem.height = rednerResult.height + 20;
+    elem.width = rednerResult.width + 20;
+    const ctx = elem.getContext("2d");
+    ctx.putImageData(image, 10, 10);
+    return [elem.height, elem.width];
+}
+
+function renderBuiltinCharacter(char = "¾", height = 200, width = 200, size = 600) {
+    const elem = getCanvasB();
+    elem.height = height;
+    elem.width = width;
+    const ctx = elem.getContext("2d");
+    ctx.font = `${size}px 'Roboto Mono'`;
+    ctx.fillText(char, 10, height - 10);
+}
+
+const input = /** @type {HTMLInputElement} */(document.getElementById("input"));
+input.addEventListener("change", function () {
+    const [height, width] = renderFontdueCharacter(this.value);
+    renderBuiltinCharacter(this.value, height, width);
+});
+const [height, width] = renderFontdueCharacter();
+renderBuiltinCharacter(undefined, height, width);
